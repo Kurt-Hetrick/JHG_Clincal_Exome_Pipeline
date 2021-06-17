df3415bd52aa894fb473019b7983b09b8e0e0ecb
Kurt Hetrick <khetric1@jhmi.edu>
2021-06-17 16:38:56 -0400

    percent cnv calls per chromosome refactored

--- scripts/E02-A01-PCT_CNV_COVERAGE_PER_CHR.sh
+++ scripts/E02-A01-PCT_CNV_COVERAGE_PER_CHR.sh
@@ -32,23 +32,27 @@
 	SM_TAG=$5
 
 	CODING_BED=$6
-		CODING_BED_NAME=$(basename $CODING_BED .bed)
-		CODING_MD5=$(md5sum $CODING_BED | cut -c 1-7)
+		CODING_BED_NAME=$(basename ${CODING_BED} .bed)
+		CODING_MD5=$(md5sum ${CODING_BED} | cut -c 1-7)
 
 # Create header in output
 
 	echo -e "CHROMOSOME\tPERCENT_COVERED_BY_CNVS" \
-	>| $CORE_PATH/$PROJECT/$FAMILY/$SM_TAG/CNV_OUTPUT/$SM_TAG".call.pct.by.chr.txt"
+	>| ${CORE_PATH}/${PROJECT}/$FAMILY/${SM_TAG}/CNV_OUTPUT/${SM_TAG}.call.pct.by.chr.txt
 
 # function to create an array for each chromosome of its length in the exomeDepth input bed file that has been merged
 
 	CREATE_CHR_LENGTH_ARRAY ()
 	{
-		CHR_LENGTH_ARRAY=(`awk 'BEGIN {OFS="\t"} $1=="'$CHROMOSOME'" {print $1,($3-$2)}' \
-			$CORE_PATH/$PROJECT/TEMP/$SM_TAG"-"$CODING_BED_NAME"-"$CODING_MD5".exomeDepth.input.merged.bed" \
-			| sort -k 2,2n \
-			| singularity exec $CNV_CONTAINER datamash \
-				-g 1 sum 2`)
+		CHR_LENGTH_ARRAY=(`awk 'BEGIN {OFS="\t"} \
+				$1=="'${CHROMOSOME}'" \
+				{print $1,($3-$2)}' \
+			${CORE_PATH}/${PROJECT}/TEMP/${SM_TAG}-${CODING_BED_NAME}-${CODING_MD5}.exomeDepth.input.merged.bed \
+			| sort \
+				-k 2,2n \
+			| singularity exec ${CNV_CONTAINER} datamash \
+				-g 1 \
+				sum 2`)
 
 			CHROMOSOME_BED=${CHR_LENGTH_ARRAY[0]}
 
@@ -61,18 +65,22 @@
 
 	CALCULATE_PCT_LENGTH ()
 	{
-		awk -v CHROMOSOME_LENGTH="$CHROMOSOME_LENGTH" 'BEGIN {OFS="\t"}  $1=="'$CHROMOSOME_BED'" {print $1,$2,$3}' \
-			$CORE_PATH/$PROJECT/$FAMILY/$SM_TAG/CNV_OUTPUT/$SM_TAG".exomeDepth.bed" \
-		| sort -k 2,2n \
-		| singularity exec $CNV_CONTAINER \
-			bedtools \
-			merge -i - \
-		| singularity exec $CNV_CONTAINER \
-			bedtools \
-			intersect \
-			-a - \
-			-b $CORE_PATH/$PROJECT/TEMP/$SM_TAG"-"$CODING_BED_NAME"-"$CODING_MD5".exomeDepth.input.merged.bed" \
-		| awk '{SUM += $3-$2} END {printf "chr" "'$CHROMOSOME_BED'" "\t" "%.1f\n", 100*SUM/"'$CHROMOSOME_LENGTH'"}'
+		awk -v CHROMOSOME_LENGTH="$CHROMOSOME_LENGTH" \
+			'BEGIN {OFS="\t"} \
+			$1=="'${CHROMOSOME_BED}'" \
+			{print $1,$2,$3}' \
+		${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CNV_OUTPUT/${SM_TAG}.exomeDepth.bed \
+			| sort -k \
+				2,2n \
+			| singularity exec ${CNV_CONTAINER} bedtools \
+				merge \
+				-i - \
+			| singularity exec ${CNV_CONTAINER} bedtools \
+				intersect \
+				-a - \
+				-b ${CORE_PATH}/${PROJECT}/TEMP/${SM_TAG}-${CODING_BED_NAME}-${CODING_MD5}.exomeDepth.input.merged.bed \
+			| awk '{SUM += $3-$2} \
+				END {printf "chr" "'${CHROMOSOME_BED}'" "\t" "%.1f\n", 100*SUM/"'${CHROMOSOME_LENGTH}'"}'
 	}
 
 # loop through each unique chromosome in the exomeDepth bed file that has been merged.
@@ -80,7 +88,7 @@
 # run both function above
 # do a natural sort of karyotype chromosomes
 
-	for CHROMOSOME in $(cut -f 1 $CORE_PATH/$PROJECT/TEMP/$SM_TAG"-"$CODING_BED_NAME"-"$CODING_MD5".exomeDepth.input.merged.bed" \
+	for CHROMOSOME in $(cut -f 1 ${CORE_PATH}/${PROJECT}/TEMP/${SM_TAG}-${CODING_BED_NAME}-${CODING_MD5}.exomeDepth.input.merged.bed \
 			| sort \
 			| uniq \
 			| egrep "^[0-9]|^X|^Y")
@@ -88,5 +96,6 @@
 		CREATE_CHR_LENGTH_ARRAY
 		CALCULATE_PCT_LENGTH
 	done \
-	| sort -k 1,1V \
-	>> $CORE_PATH/$PROJECT/$FAMILY/$SM_TAG/CNV_OUTPUT/$SM_TAG".call.pct.by.chr.txt"
+	| sort \
+		-k 1,1V \
+	>> ${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CNV_OUTPUT/${SM_TAG}.call.pct.by.chr.txt
