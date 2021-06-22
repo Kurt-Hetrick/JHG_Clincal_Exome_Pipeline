77aebe721e5707c8d7687fcf1def456ad1f4aab0
Kurt Hetrick <khetric1@jhmi.edu>
2021-06-22 16:12:41 -0400

    eklipe refactored

--- scripts/E04-A01-RUN_EKLIPSE.sh
+++ scripts/E04-A01-RUN_EKLIPSE.sh
@@ -34,7 +34,7 @@
 	THREADS=$7
 
 	SAMPLE_SHEET=$8
-		SAMPLE_SHEET_NAME=$(basename $SAMPLE_SHEET .csv)
+		SAMPLE_SHEET_NAME=$(basename ${SAMPLE_SHEET} .csv)
 	SUBMIT_STAMP=$9
 
 ## --extract out the MT reads in the bam file
@@ -43,42 +43,42 @@ START_RUN_EKLIPSE=`date '+%s'` # capture time process starts for wall clock trac
 
 	# construct command line
 
-		CMD="singularity exec $MITO_EKLIPSE_CONTAINER eKLIPse.py" \
-		CMD=$CMD" -in $CORE_PATH/$PROJECT/TEMP/$SM_TAG"_EKLIPSE_CONFIG.txt"" \
-		CMD=$CMD" -ref $MT_GENBANK" \
+		CMD="singularity exec ${MITO_EKLIPSE_CONTAINER} eKLIPse.py" \
+		CMD=$CMD" -in ${CORE_PATH}/${PROJECT}/TEMP/${SM_TAG}_EKLIPSE_CONFIG.txt" \
+		CMD=$CMD" -ref ${MT_GENBANK}" \
 		CMD=$CMD" -thread $THREADS" \
 		CMD=$CMD" -downcov 0" \
 		CMD=$CMD" -scsize 15" \
 		CMD=$CMD" -mapsize 10" \
-		CMD=$CMD" -out $CORE_PATH/$PROJECT/$FAMILY/$SM_TAG/MT_OUTPUT/EKLIPSE"
+		CMD=$CMD" -out ${CORE_PATH}/${PROJECT}/$FAMILY/${SM_TAG}/MT_OUTPUT/EKLIPSE"
 
 	# write command line to file and execute the command line
 
-		echo $CMD >> $CORE_PATH/$PROJECT/COMMAND_LINES/$SM_TAG"_command_lines.txt"
-		echo >> $CORE_PATH/$PROJECT/COMMAND_LINES/$SM_TAG"_command_lines.txt"
-		echo $CMD | bash
+		echo ${CMD} >> ${CORE_PATH}/${PROJECT}/COMMAND_LINES/${SM_TAG}_command_lines.txt
+		echo >> ${CORE_PATH}/${PROJECT}/COMMAND_LINES/${SM_TAG}_command_lines.txt
+		echo ${CMD} | bash
 
 	# check the exit signal at this point.
 
 		SCRIPT_STATUS=`echo $?`
 
-	# if exit does not equal 0 then exit with whatever the exit signal is at the end.
-	# also write to file that this job failed
+		# if exit does not equal 0 then exit with whatever the exit signal is at the end.
+		# also write to file that this job failed
 
-		if [ "$SCRIPT_STATUS" -ne 0 ]
-		 then
-			echo $SM_TAG $HOSTNAME $JOB_NAME $USER $SCRIPT_STATUS $SGE_STDERR_PATH \
-			>> $CORE_PATH/$PROJECT/TEMP/$SAMPLE_SHEET_NAME"_"$SUBMIT_STAMP"_ERRORS.txt"
-			exit $SCRIPT_STATUS
-		fi
+			if [ "${SCRIPT_STATUS}" -ne 0 ]
+				then
+					echo ${SM_TAG} ${HOSTNAME} ${JOB_NAME} ${USER} ${SCRIPT_STATUS} ${SGE_STDERR_PATH} \
+					>> ${CORE_PATH}/${PROJECT}/TEMP/${SAMPLE_SHEET_NAME}_${SUBMIT_STAMP}_ERRORS.txt
+					exit ${SCRIPT_STATUS}
+			fi
 
 END_RUN_EKLIPSE=`date '+%s'` # capture time process starts for wall clock tracking purposes.
 
 # write out timing metrics to file
 
-	echo $SM_TAG"_"$PROJECT",F.01,RUN_EKLIPSE,"$HOSTNAME","$START_RUN_EKLIPSE","$END_RUN_EKLIPSE \
-	>> $CORE_PATH/$PROJECT/REPORTS/$PROJECT".WALL.CLOCK.TIMES.csv"
+	echo ${SM_TAG}_${PROJECT},F01,RUN_EKLIPSE,${HOSTNAME},${START_RUN_EKLIPSE},${END_RUN_EKLIPSE} \
+	>> ${CORE_PATH}/${PROJECT}/REPORTS/${PROJECT}.WALL.CLOCK.TIMES.csv
 
 # exit with the signal from samtools bam to cram
 
-	exit $SCRIPT_STATUS
+	exit ${SCRIPT_STATUS}
