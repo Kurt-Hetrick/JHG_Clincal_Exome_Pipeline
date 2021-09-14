28f67fcf18da3e4ce33ed8e6e1e064bfe9e56576
Kurt Hetrick <khetric1@jhmi.edu>
2021-09-14 10:32:01 -0400

    some cleanup

--- scripts/E03-A02-MUTECT2_MT_BAM_TO_CRAM.sh
+++ scripts/E03-A02-MUTECT2_MT_BAM_TO_CRAM.sh
@@ -39,7 +39,7 @@
 
 ## --write lossless cram file. this is the deliverable
 
-START_CRAM=`date '+%s'` # capture time process starts for wall clock tracking purposes.
+START_MUTECT2_CRAM=`date '+%s'` # capture time process starts for wall clock tracking purposes.
 
 	# construct command line
 
@@ -72,101 +72,12 @@ START_CRAM=`date '+%s'` # capture time process starts for wall clock tracking pu
 					exit ${SCRIPT_STATUS}
 			fi
 
-END_CRAM=`date '+%s'` # capture time process stops for wall clock tracking purposes.
+END_MUTECT2_CRAM=`date '+%s'` # capture time process stops for wall clock tracking purposes.
 
 # write out timing metrics to file
 
-	echo ${SM_TAG}_${PROJECT},E01,CRAM,${HOSTNAME},${START_CRAM},${END_CRAM} \
+	echo ${SM_TAG}_${PROJECT},E01,CRAM,${HOSTNAME},${START_MUTECT2_CRAM},${END_MUTECT2_CRAM} \
 	>> ${CORE_PATH}/${PROJECT}/REPORTS/${PROJECT}.WALL.CLOCK.TIMES.csv
-
-###############################################################################################################
-## grab the read group header from the cram file and a make stub for the meta data ############################
-## this is so if you need to regenerate a qc report and the cram file has been moved and not symlinked back ###
-## this can be reference to use in generating the qc report. ##################################################
-###############################################################################################################
-#### THIS IS THE HEADER ################################################
-#### "SM_TAG","PROJECT","RG_PU","LIBRARY" ##############################
-#### "LIBRARY_PLATE","LIBRARY_WELL","LIBRARY_ROW","LIBRARY_COLUMN" #####
-#### "HYB_PLATE","HYB_WELL","HYB_ROW","HYB_COLUMN" #####################
-########################################################################
-
-	if [ -f ${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram ];
-		then
-
-			# grab field number for SM_TAG
-
-				SM_FIELD=(`singularity exec ${ALIGNMENT_CONTAINER} samtools \
-					view -H \
-				${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram \
-					| grep -m 1 ^@RG \
-					| sed 's/\t/\n/g' \
-					| cat -n \
-					| sed 's/^ *//g' \
-					| awk '$2~/^SM:/ \
-						{print $1}'`)
-
-			# grab field number for PLATFORM_UNIT_TAG
-
-				PU_FIELD=(`singularity exec ${ALIGNMENT_CONTAINER} samtools \
-					view -H \
-				${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram \
-					| grep -m 1 ^@RG \
-					| sed 's/\t/\n/g' \
-					| cat -n \
-					| sed 's/^ *//g' \
-					| awk '$2~/^PU:/ \
-						{print $1}'`)
-
-			# grab field number for LIBRARY_TAG
-
-				LB_FIELD=(`singularity exec ${ALIGNMENT_CONTAINER} samtools \
-					view -H \
-				${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram \
-					| grep -m 1 ^@RG \
-					| sed 's/\t/\n/g' \
-					| cat -n \
-					| sed 's/^ *//g' \
-					| awk '$2~/^LB:/ \
-						{print $1}'`)
-
-			# grab field number for PROGRAM_TAG
-
-				PG_FIELD=(`singularity exec ${ALIGNMENT_CONTAINER} samtools \
-					view -H \
-				${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram \
-					| grep -m 1 ^@RG \
-					| sed 's/\t/\n/g' \
-					| cat -n \
-					| sed 's/^ *//g' \
-					| awk '$2~/^PG:/ \
-						{print $1}'`)
-
-			# Now grab the header and format
-				# breaking out the library name into its parts is assuming that the format is...
-				# fill in empty fields with NA thing (for loop in awk) is a lifesaver
-				# https://unix.stackexchange.com/questions/53448/replacing-missing-value-blank-space-with-zero
-
-				singularity exec ${ALIGNMENT_CONTAINER} samtools \
-					view -H \
-				${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/CRAM/${SM_TAG}.cram \
-					| grep ^@RG \
-					| awk \
-						-v SM_FIELD="$SM_FIELD" \
-						-v PU_FIELD="$PU_FIELD" \
-						-v LB_FIELD="$LB_FIELD" \
-						-v PG_FIELD="$PG_FIELD" \
-						'BEGIN {OFS="\t"} \
-						{split($SM_FIELD,SMtag,":"); split($PU_FIELD,PU,":"); \
-						split($LB_FIELD,Library,":"); split($PG_FIELD,Pipeline,":"); \
-						print "'${PROJECT}'",SMtag[2],PU[2],Library[2],Pipeline[2]}' \
-					| awk 'BEGIN { FS = OFS = "\t" } \
-						{ for(i=1; i<=NF; i++) if($i ~ /^ *$/) $i = "NA" }; 1' \
-				>| ${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/REPORTS/RG_HEADER/${SM_TAG}.RG_HEADER.txt
-		else
-			echo -e "${PROJECT}\t${SM_TAG}\tNA\tNA\tNA" \
-			| $DATAMASH_DIR/datamash transpose \
-			>| ${CORE_PATH}/${PROJECT}/${FAMILY}/${SM_TAG}/REPORTS/RG_HEADER/${SM_TAG}.RG_HEADER.txt
-	fi
 
 # exit with the signal from samtools bam to cram
 
